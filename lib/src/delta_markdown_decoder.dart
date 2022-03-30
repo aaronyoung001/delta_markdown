8b12403f976ef015aea97278c5f50da836751075
aaronyoung001 <love.further@qq.com>
2022-03-30 15:48:42 +0800

    Update delta_markdown_decoder.dart

--- lib/src/delta_markdown_decoder.dart
+++ lib/src/delta_markdown_decoder.dart
@@ -1,7 +1,7 @@
 import 'dart:collection';
 import 'dart:convert';
 
-import 'package:flutter_quill/flutter_quill.dart'
+import 'package:flutter_quill/flutter_quill.dart';
 
 import 'ast.dart' as ast;
 import 'document.dart';
